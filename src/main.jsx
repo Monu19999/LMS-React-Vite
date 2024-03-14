a5d965b1dc6c564b876a8a9f7c2a3baaee14531d
Harshwardhan <hw.sharma9@gmail.com>
2024-03-14 15:24:17 +0530

    HashRouter used

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@src/App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 // import "/assets/css/bootstrap.min.css";
 // import "/assets/css/style.css";
 // import "/assets/css/change.css";
