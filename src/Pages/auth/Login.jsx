be5005babf2c5e1184b29f4110e1971ca04ade46
Harshwardhan <hw.sharma9@gmail.com>
2024-05-06 13:13:17 +0530

    login bug fixing

--- src/Pages/auth/Login.jsx
+++ src/Pages/auth/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
             : {
                   defaultValues: {
                       email: "hw.sharma9@mp.gov.in",
-                      password: "Password",
+                      password: "password",
                   },
               };
     const {
